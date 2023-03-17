81d8cea3ef5bcc731842bb49b0a816cdd26a310f
SoyYo111 <125015232+SoyYo111@users.noreply.github.com>
2023-03-17 10:32:09 +0100

    Update triangulo.java

--- triangulo.java
+++ triangulo.java
@@ -1,11 +1,22 @@
-package entornos2;
+package Practica2;
 
 import java.util.*;
 
 class Triangulo{
     public static void main(String [] args){
+         
         double base, altura, resultado;     
         Scanner teclado = new Scanner(System.in);
+         
         System.out.print("Ingrese base:\t");
         base = teclado.nextFloat();
          
+        System.out.print("Ingrese altura:\t");
+        altura = teclado.nextFloat();
+         
+        resultado = ((base*altura)/2);
+         
+        System.out.print("El area del Triangulo es:\t"+resultado);
+        teclado.close();
+    }
+}
