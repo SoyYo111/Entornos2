cda01ae60af1f29b2087b739735245599433c55e
SoyYo111 <forglow321@gmail.com>
2023-03-17 09:55:48 +0100

    variables

--- cuadrado.java
+++ cuadrado.java
@@ -2,3 +2,8 @@ package entornos2;
 import java.util.Scanner;
 
 public class Practica2 {
+    public static void main(String[] args) 
+	{
+		 Scanner teclado = new Scanner( System.in );
+		// TODO Auto-generated method stub
+		 int num,cuadrado;
