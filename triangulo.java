36f1d524f4df856a866a60b83ee733a350c3c80c
SoyYo111 <forglow321@gmail.com>
2023-03-17 10:02:19 +0100

    pedir base

--- triangulo.java
+++ triangulo.java
@@ -6,4 +6,6 @@ class Triangulo{
     public static void main(String [] args){
         double base, altura, resultado;     
         Scanner teclado = new Scanner(System.in);
-        
+        System.out.print("Ingrese base:\t");
+        base = teclado.nextFloat();
+         
