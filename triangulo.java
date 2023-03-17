3b536dac3d2af4ef2d085d3e20d29f7c0e302027
SoyYo111 <forglow321@gmail.com>
2023-03-17 10:01:52 +0100

    variables triangulo

--- triangulo.java
+++ triangulo.java
@@ -4,3 +4,6 @@ import java.util.*;
 
 class Triangulo{
     public static void main(String [] args){
+        double base, altura, resultado;     
+        Scanner teclado = new Scanner(System.in);
+        
