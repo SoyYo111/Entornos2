8c635af11a053b1f65cb66934a5823264d742ade
SoyYo111 <125015232+SoyYo111@users.noreply.github.com>
2023-03-21 12:04:28 +0100

    Update cuadrado.java

--- cuadrado.java
+++ cuadrado.java
@@ -5,7 +5,7 @@ public class Practica2 {
 
 	public static void main(String[] args) 
 	{
-		 Scanner teclado = new Scanner( System.in );
+		 Scanner teclado5 = new Scanner( System.in );
 
  TODO Auto-generated method stub1
 
